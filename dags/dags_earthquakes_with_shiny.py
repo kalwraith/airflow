051e6ad376bb62e19f31cdc69877dc6bdc286cda
hyunjinkim <kalwraith@naver.com>
2023-02-14 18:07:18 +0900

    jinja 변수 수정

--- dags/dags_earthquakes_with_shiny.py
+++ dags/dags_earthquakes_with_shiny.py
@@ -3,23 +3,23 @@ from airflow.operators.python import PythonOperator
 from src_shiny.earthquakes import Earthquakes
 from datetime import datetime
 
-dag = DAG(dag_id='dags_earthquakes_with_shiny',
-          start_date=datetime(2023,2,14),
+xxxx = DAG(dag_id='dags_earthquakes_with_shiny',
+          start_date=datetime(2023,2,12),
           schedule_interval='0 1 * * *')
 
 earthquakes = Earthquakes()
 t1 = PythonOperator(
     task_id='delete_earthquakes',
     python_callable=earthquakes.del_data,
-    op_kwargs={'starttime': "{{ prev_ds }}", 'endtime': "{{ next_ds }}"},
-    dag=dag
+    op_kwargs={'starttime': "{{ data_interval_start | ds }}", 'endtime': "{{ data_interval_end | ds }}"},
+    dag=xxxx
 )
 
 t2 = PythonOperator(
     task_id='insert_earthquakes',
     python_callable=earthquakes.insrt_data,
-    op_kwargs={'starttime': "{{ prev_ds }}", 'endtime': "{{ next_ds }}"},
-    dag=dag
+    op_kwargs={'starttime': "{{ data_interval_start | ds }}", 'endtime': "{{ data_interval_end | ds }}"},
+    dag=xxxx
 )
 
 t1 >> t2
