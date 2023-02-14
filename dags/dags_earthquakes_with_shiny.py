3c1922551bef4d196a0592955c51a1acebb88825
unknown <hyunjinkim@DESKTOP-2JF5L03.lgcns.com>
2023-02-14 17:55:39 +0900

    dag start date 추가

--- dags/dags_earthquakes_with_shiny.py
+++ dags/dags_earthquakes_with_shiny.py
@@ -1,8 +1,10 @@
 from airflow.models import DAG
 from airflow.operators.python import PythonOperator
 from src_shiny.earthquakes import Earthquakes
+from datetime import datetime
 
 dag = DAG(dag_id='dags_earthquakes_with_shiny',
+          start_date=datetime(2023,2,14),
           schedule_interval='0 1 * * *')
 
 earthquakes = Earthquakes()
