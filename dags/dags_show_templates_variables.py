bc231486f2e97dd500615e46fd245e23ac097bc3
hyunjinkim <kalwraith@naver.com>
2023-02-16 16:29:46 +0900

    template 변수 dag 추가

---
+++ dags/dags_show_templates_variables.py
@@ -0,0 +1,25 @@
+# Package Import
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+dag = DAG(dag_id='dags_show_templates_variables',
+          start_date=datetime(2023,2,16),
+          schedule_interval='0 1 * * *')
+
+t1 = BashOperator(
+    task_id='show_template_variables',
+    bash_command='echo {{ dag_run.logical_date | ds }}; \
+     echo {{ dag_run.logical_date | ds_nodash }}; \
+     echo {{ dag_run.logical_date | ts }}; \
+     echo {{ prev_data_interval_start_success | ds }}; \
+     echo {{ prev_data_interval_end_success | ds }}; \
+     echo {{ prev_start_date_success | ds }}; \
+     echo {{ dag }}; \
+     echo {{ task }}; \
+     echo {{ macros }}; \
+     echo {{ task_instance }};',
+    dag=earthquakes_dag
+)
+
+t1
