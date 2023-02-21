fafd42cbc4d3296e746be19d33a300de9d431024
hyunjinkim <kalwraith@naver.com>
2023-02-21 14:14:12 +0900

    catchup=False 추가'

--- dags/dags_base_branch_operator.py
+++ dags/dags_base_branch_operator.py
@@ -7,11 +7,15 @@ from datetime import datetime
 
 dag = DAG(dag_id='dags_base_branch_operator',
           start_date=datetime(2023,2,20),
-          schedule_interval='*/5 * * * *')
+          schedule_interval='*/5 * * * *',
+          catchup=False
+          )
 
 
 class BaseBashOperator(BaseBranchOperator):
     def choose_branch(self, context):
+        #context['ts'] 는 string 타입의 %Y-%m-%dT%H:%M:%S+00:00 을 반환함
+
         ts = datetime.strptime(context['ts'], '%Y-%m-%dT%H:%M:%S+00:00')
         if int(divmod(ts.minute,2)[1]) == 0:
             return 'bash_branch_task1'
