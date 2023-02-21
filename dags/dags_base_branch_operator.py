5fc10c3c861a0ad4753347d26aa0cdbbf1328aad
hyunjinkim <kalwraith@naver.com>
2023-02-21 13:36:46 +0900

    base_branch_operator

---
+++ dags/dags_base_branch_operator.py
@@ -0,0 +1,36 @@
+# Package Import
+from airflow.models import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.branch import BaseBranchOperator
+from airflow.operators.empty import EmptyOperator
+from datetime import datetime
+
+dag = DAG(dag_id='dags_base_branch_operator',
+          start_date=datetime(2023,2,20),
+          schedule_interval='0 1 * * *')
+
+
+class BaseBashOperator(BaseBranchOperator):
+    def choose_branch(self, context: Context) -> str | Iterable[str]:
+        if context['data_interval_start'].day == 21:
+            return 'bash_branch_task1'
+        else:
+            return 'empty_task_1'
+
+
+bash_branch_task1 = BaseBranchOperator(
+    task_id='bash_branch_task1',
+    bash_command='/opt/airflow/plugins/shell/select_fruit.sh Orange',
+    dag=dag
+)
+
+empty_task_1 = EmptyOperator(
+    task_id='empty_task_1'
+)
+
+base_branch_operator = BaseBashOperator(
+    task_id='base_branch_operator',
+    dag=dag
+)
+
+base_branch_operator >> [bash_branch_task1, empty_task_1]
