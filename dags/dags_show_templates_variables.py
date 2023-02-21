2770925473ae5cb13350c31b4428ef87bd5c2032
hyunjinkim <kalwraith@naver.com>
2023-02-21 14:13:57 +0900

    스케줄 제거, template 변수 추가'

--- dags/dags_show_templates_variables.py
+++ dags/dags_show_templates_variables.py
@@ -5,13 +5,18 @@ from datetime import datetime
 
 dag = DAG(dag_id='dags_show_templates_variables',
           start_date=datetime(2023,2,16),
-          schedule_interval='0 1 * * *')
+          schedule_interval=None)
 
 t1 = BashOperator(
     task_id='show_template_variables',
-    bash_command='echo "dag_run.logical_date | ds: {{ dag_run.logical_date | ds }}"; \
+    bash_command='echo "data_interval_start | ds: {{ data_interval_start }}"; \
+     echo "data_interval_end | ds: {{ data_interval_end }}"; \
+     echo "dag_run.logical_date | ds: {{ dag_run.logical_date | ds }}"; \
      echo "dag_run.logical_date | ds_nodash: {{ dag_run.logical_date | ds_nodash }}"; \
      echo "dag_run.logical_date | ts: {{ dag_run.logical_date | ts }}"; \
+     echo "ts: {{ ts }}"; \
+     echo "ts_nodash_with_tz: {{ ts_nodash_with_tz }}"; \
+     echo "ts_nodash: {{ ts_nodash }}"; \
      echo "prev_data_interval_start_success | ds: {{ prev_data_interval_start_success | ds }}"; \
      echo "prev_data_interval_end_success | ds: {{ prev_data_interval_end_success | ds }}"; \
      echo "prev_start_date_success | ds: {{ prev_start_date_success | ds }}"; \
