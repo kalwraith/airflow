a6b4d519d1e6ab36e920383eedf88858950efb49
hyunjinkim <kalwraith@naver.com>
2023-02-21 15:15:35 +0900

    operator 추가

--- dags/dag_creation/dags_creation_by_decorator.py
+++ dags/dag_creation/dags_creation_by_decorator.py
@@ -3,13 +3,20 @@ from datetime import datetime
 from airflow.operators.empty import EmptyOperator
 from airflow.decorators import dag
 
-# 데코레이터 방식
+# 데코레이터 방식 (dag_id는 별도로 정의하지 않으며 함수명이 곧 dag_id가 된다)
 @dag(start_date=datetime(2023,2,20),
      schedule=None,
      catchup=False
      )
-def generate_dag():
-    empty_task = EmptyOperator(
-        task_id='empty_task'
+def dags_creation_by_decorator():
+    empty_task_1 = EmptyOperator(
+        task_id='empty_task_1'
     )
-dag = generate_dag()
+
+    empty_task_2 = EmptyOperator(
+        task_id='empty_task_2'
+    )
+
+    empty_task_1 >> empty_task_2
+
+dag = dags_creation_by_decorator()
