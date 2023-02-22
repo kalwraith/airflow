12ad7d1242cfbcd7dc6b6dfc565d1363c90fcf5d
hyunjinkim <kalwraith@naver.com>
2023-02-22 11:22:40 +0900

    branch_python operator kwargs 수정

--- dags/dags_branch_python_operator.py
+++ dags/dags_branch_python_operator.py
@@ -11,9 +11,8 @@ def select_random():
     return task_lst[select]
 
 
-def common_func(**context):
-    params = context['params']
-    print(params['selected'])
+def common_func(**kwargs):
+    print(kwargs['selected'])
 
 with DAG(
     dag_id='dags_branch_python_operator',
@@ -35,25 +34,25 @@ with DAG(
     task_a = PythonOperator(
         task_id='task_a',
         python_callable=common_func,
-        params={'selected':'A'}
+        op_kwargs={'selected':'A'}
     )
 
     task_b = PythonOperator(
         task_id='task_b',
         python_callable=common_func,
-        params={'selected':'B'}
+        op_kwargs={'selected':'B'}
     )
 
     task_c = PythonOperator(
         task_id='task_c',
         python_callable=common_func,
-        params={'selected':'C'}
+        op_kwargs={'selected':'C'}
     )
 
     task_d = PythonOperator(
         task_id='task_d',
         python_callable=common_func,
-        params={'selected':'D'}
+        op_kwargs={'selected':'D'}
     )
 
     start_task >> python_branch_task >> [task_a, task_b, task_c, task_d]
