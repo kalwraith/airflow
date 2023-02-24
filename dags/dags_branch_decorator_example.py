fba9dbda9aebac5a8b62d7bd093fd5d4656a3d6f
hyunjinkim <kalwraith@naver.com>
2023-02-24 14:17:45 +0900

    branch decorator upload

---
+++ dags/dags_branch_decorator_example.py
@@ -0,0 +1,38 @@
+# Package Import
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.decorators import task
+from datetime import datetime
+
+with DAG(dag_id='dags_branch_decorator_example',
+          start_date=datetime(2023,2,16),
+          schedule_interval='0 1 * * *'
+) as dag:
+
+    @task.branch(task_id='branching')
+    def random_branch():
+        task_lst = ['task_a', 'task_b', 'task_c', 'task_d']
+        select = random.randint(0, 3)
+        return task_lst[select]
+
+    task_a = BashOperator(
+    task_id='task_a',
+    bash_command='echo "Selected Task A!"'
+    )
+
+    task_b = BashOperator(
+        task_id='task_b',
+        bash_command='echo "Selected Task B!"'
+    )
+
+    task_c = BashOperator(
+        task_id='task_c',
+        bash_command='echo "Selected Task C!"'
+    )
+
+    task_d = BashOperator(
+        task_id='task_d',
+        bash_command='echo "Selected Task D!"'
+    )
+
+    branching >> [task_a,task_b,task_c,task_d]
