fe755b0e08f1cd1698ae3492a4f10965152d6ff7
hyunjinkim <kalwraith@naver.com>
2023-02-27 15:54:57 +0900

    내용 추가

--- dags/dags_xcom_usage_with_bash.py
+++ dags/dags_xcom_usage_with_bash.py
@@ -4,14 +4,17 @@ from airflow.operators.python import PythonOperator
 import pendulum
 
 with DAG(
-    dag_id='dags_xcom_usage',
+    dag_id='dags_xcom_usage_with_bash',
     start_date=pendulum.datetime(2023,2,1,tz='Asia/Seoul'),
     schedule=None,
     catchup=False
 ) as dag:
     bash_task_1 = BashOperator(
         task_id='bash_task_1',
-        bash_command='/opt/airflow/plugins/shell/select_fruit.sh Orange',
+        bash_command="echo {{ ti }}; " +
+                     "/opt/airflow/plugins/shell/select_fruit.sh Orange ; " +
+                     "{{ ti.xcom_push(key='Fruit_type', value='ORANGE' }}"
+
     )
 
     bash_task_2 = BashOperator(
