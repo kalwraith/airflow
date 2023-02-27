147b20974566416ed730746977194fab4add824d
hyunjinkim <kalwraith@naver.com>
2023-02-27 18:02:37 +0900

    earthquake 쿼리 수정:

--- dags/dags_earthquakes_with_shiny.py
+++ dags/dags_earthquakes_with_shiny.py
@@ -2,35 +2,30 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
-from datetime import datetime
+import pendulum
 
 ## Custom Import
 from src_shiny.earthquakes import Earthquakes
 
-earthquakes_dag = DAG(dag_id='dags_earthquakes_with_shiny',
-          start_date=datetime(2023,2,12),
-          schedule_interval='0 1 * * *')
+with DAG(dag_id='dags_earthquakes_with_shiny',
+          start_date=pendulum.datetime(2023,2,12, tz='Asia/Seoul'),
+          schedule_interval='0 1 * * *'
+         ) as dag:
 
-earthquakes = Earthquakes()
-t1 = PythonOperator(
-    task_id='delete_earthquakes',
-    python_callable=earthquakes.del_data,
-    op_kwargs={'starttime': "{{ data_interval_start | ds }}", 'endtime': "{{ data_interval_end | ds }}"},
-    dag=earthquakes_dag
-)
+    earthquakes = Earthquakes()
+    t1 = PythonOperator(
+        task_id='delete_earthquakes',
+        python_callable=earthquakes.del_data,
+    )
 
-t2 = PythonOperator(
-    task_id='insert_earthquakes',
-    python_callable=earthquakes.insrt_data,
-    op_kwargs={'starttime': "{{ data_interval_start | ds }}", 'endtime': "{{ data_interval_end | ds }}"},
-    dag=earthquakes_dag
-)
+    t2 = PythonOperator(
+        task_id='insert_earthquakes',
+        python_callable=earthquakes.insrt_data,
+    )
 
+    t3 = BashOperator(
+        task_id='log_task_result',
+        bash_command='echo "good nice" > /opt/airflow/plugins/{{ data_interval_end | ds }}.log',
+    )
 
-t3 = BashOperator(
-    task_id='log_task_result',
-    bash_command='echo "good nice" > /opt/airflow/plugins/{{ data_interval_end | ds }}.log',
-    dag=earthquakes_dag
-)
-
-t1 >> t2 >> t3
+    t1 >> t2 >> t3
