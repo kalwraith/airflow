1a486a0acceef3d444ebe0fc72cea3821f94e145
hyunjinkim <kalwraith@naver.com>
2023-02-28 17:34:18 +0900

    macro 사용방법

--- dags/dags_macro_with_bash.py
+++ dags/dags_macro_with_bash.py
@@ -1,16 +1,23 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 import pendulum
 
 with DAG(
     dag_id='dags_macro_with_bash',
     start_date=pendulum.datetime(2023,2,1, tz='Asia/Seoul'),
-    schedule=None,
+    schedule='10 1 * * 6#1',
     catchup=False
 ) as dag:
-    task_1 = BashOperator(
+
+    def function_for_prev_month(start_date, end_date):
+        print(f'기간 처리:{start_date} ~ {end_date}')
+
+    task_1 = PythonOperator(
         task_id='task_1',
-        bash_command='echo "You can calculate date: {{ data_interval_end + macros.dateutil.relativedelta.relativedelta(month=-1, day=1) }}"'
+        templates_dict={'start_date':'{{ data_interval_end - macros.dateutil.relativedelta.relativedelta(months=-1, day=1) }}',
+                        'end_date': '{{ data_interval_end - macros.dateutil.relativedelta.relativedelta(day=1) - macros.dateutil.relativedelta.relativedelta(days=1)}}'
+                        },
+        python_callable=function_for_prev_month
     )
 
     task_1
