2f282bee05dec62069c6effd1e100bcb38804cf7
hyunjinkim <kalwraith@naver.com>
2023-03-02 15:35:20 +0900

    날짜 오류 확인 중

--- dags/dags_macro_with_bash.py
+++ dags/dags_macro_with_bash.py
@@ -13,10 +13,18 @@ with DAG(
     # 2주 전 월요일부터 토요일까지 날짜 가져오기
     task_1 = BashOperator(
         task_id='task_1',
-        env={'START_DATE':'{{ (data_interval_end + macros.dateutil.relativedelta.relativedelta(days=-13)) | ds }}',
-             'END_DATE':'{{ (data_interval_end + macros.dateutil.relativedelta.relativedelta(days=-8)) | ds }}'},
-        bash_command='echo "start_date: $START_DATE" && '
-                     'echo "end_date: $END_DATE" '
+        env={'D_8':'{{ (data_interval_end + macros.dateutil.relativedelta.relativedelta(days=-8)) | ds }}',
+             'D_9':'{{ (data_interval_end + macros.dateutil.relativedelta.relativedelta(days=-9)) | ds }}',
+             'D_10': '{{ (data_interval_end + macros.dateutil.relativedelta.relativedelta(days=-10)) | ds }}',
+             'D_11': '{{ (data_interval_end + macros.dateutil.relativedelta.relativedelta(days=-11)) | ds }}',
+             'D_12': '{{ (data_interval_end + macros.dateutil.relativedelta.relativedelta(days=-12)) | ds }}',
+             'D_13': '{{ (data_interval_end + macros.dateutil.relativedelta.relativedelta(days=-13)) | ds }}'},
+        bash_command='echo "d-8: $D_8" && '
+                     'echo "d-9: $D_9" && '
+                     'echo "d-10: $D_10" && '
+                     'echo "d-11: $D_11" && '
+                     'echo "d-12: $D_12" && '
+                     'echo "d-13: $D_13" '
 
     )
 
