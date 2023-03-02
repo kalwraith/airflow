8c2402842b42f4894473c12ea5ab3c1d0f9b86c7
hyunjinkim <kalwraith@naver.com>
2023-03-02 15:22:46 +0900

    env 변수 추가

--- dags/dags_macro_with_bash.py
+++ dags/dags_macro_with_bash.py
@@ -1,19 +1,22 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+
 import pendulum
 
 with DAG(
     dag_id='dags_macro_with_bash',
     start_date=pendulum.datetime(2023,2,1, tz='Asia/Seoul'),
-    schedule='10 1 * * 0#1',
+    schedule='10 1 * * 0#2',
     catchup=False
 ) as dag:
 
-    # 전월 마지막주 월요일부터 토요일까지 날짜 가져오기
+    # 2주 전 월요일부터 토요일까지 날짜 가져오기
     task_1 = BashOperator(
         task_id='task_1',
-        bash_command='echo "start_date: {{ (data_interval_end + macros.dateutil.relativedelta.relativedelta(months=-1, weekday=MO(-1)) | ds) }}" && '
-                     'echo "end_date: {{ (data_interval_end + macros.dateutil.relativedelta.relativedelta(months=-1, weekday=SA(-1)) | ds ) }}" '
+        env={'START_DATE':'{{ (data_interval_end + macros.dateutil.relativedelta.relativedelta(days=-13) }}',
+             'END_DATE':'{{ (data_interval_end + macros.dateutil.relativedelta.relativedelta(days=-8) | ds ) }}'},
+        bash_command='echo "start_date: $START_DATE" && '
+                     'echo "end_date: $END_DATE" '
 
     )
 
