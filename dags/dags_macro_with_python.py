f3fcf35d69315c95c6a1aae66d93c1f775e2cf0d
hyunjinkim <kalwraith@naver.com>
2023-03-02 10:14:04 +0900

    오류 수정'

--- dags/dags_macro_with_python.py
+++ dags/dags_macro_with_python.py
@@ -3,7 +3,7 @@ from airflow.operators.python import PythonOperator
 import pendulum
 
 with DAG(
-    dag_id='dags_macro_with_bash',
+    dag_id='dags_macro_with_python',
     start_date=pendulum.datetime(2023,2,1, tz='Asia/Seoul'),
     schedule='10 1 * * 6#1',
     catchup=False
@@ -14,8 +14,8 @@ with DAG(
 
     task_1 = PythonOperator(
         task_id='task_1',
-        templates_dict={'start_date':'{{ data_interval_end - macros.dateutil.relativedelta.relativedelta(months=-1, day=1) }}',
-                        'end_date': '{{ data_interval_end - macros.dateutil.relativedelta.relativedelta(day=1) - macros.dateutil.relativedelta.relativedelta(days=1)}}'
+        templates_dict={'start_date':'{{ data_interval_end + macros.dateutil.relativedelta.relativedelta(months=-1, day=1) }}',
+                        'end_date': '{{ data_interval_end - macros.dateutil.relativedelta.relativedelta(day=1) + macros.dateutil.relativedelta.relativedelta(days=-1)}}'
                         },
         python_callable=function_for_prev_month
     )
