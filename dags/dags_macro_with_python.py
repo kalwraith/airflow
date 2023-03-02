b935cc224155b95eb06376527ed56f4d543024f4
hyunjinkim <kalwraith@naver.com>
2023-03-02 10:38:55 +0900

    수정 완료:

--- dags/dags_macro_with_python.py
+++ dags/dags_macro_with_python.py
@@ -10,13 +10,13 @@ with DAG(
 ) as dag:
 
     def function_for_prev_month(**kwargs):
-        template_dict = kwargs.get('template_dict') or {}
+        template_dict = kwargs.get('templates_dict') or {}
         if template_dict:
             start_date = template_dict.get('start_date') or 'start_date없음'
             end_date = template_dict.get('end_date') or 'end_date없음'
             print(f'기간 처리:{start_date} ~ {end_date}')
         else:
-            print('template_dict 파라미터가 없습니다')
+            print('templates_dict 파라미터가 없습니다')
 
     # 전월 1일 부터 말일까지 가져오기
     task_1 = PythonOperator(
