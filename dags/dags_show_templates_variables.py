e14c7f7437b1b3e11a402eac45e1f1b750f2894f
hyunjinkim <kalwraith@naver.com>
2023-03-06 16:14:01 +0900

    show 수정

--- dags/dags_show_templates_variables.py
+++ dags/dags_show_templates_variables.py
@@ -1,31 +1,33 @@
 # Package Import
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from datetime import datetime
+import pendulum
 
-dag = DAG(dag_id='dags_show_templates_variables',
-          start_date=datetime(2023,2,16),
-          schedule_interval=None)
+with DAG(
+        dag_id='dags_show_templates_variables',
+        start_date=datetime(2023,2,16),
+        schedule_interval='10 0 * * *',
+        catchup=False) as dag:
 
-t1 = BashOperator(
-    task_id='show_template_variables',
-    bash_command='\
-        echo "data_interval_start | ds: {{ data_interval_start }}"; \
-        echo "data_interval_end | ds: {{ data_interval_end }}"; \
-        echo "dag_run.logical_date | ds: {{ dag_run.logical_date | ds }}"; \
-        echo "dag_run.logical_date | ds_nodash: {{ dag_run.logical_date | ds_nodash }}"; \
-        echo "dag_run.logical_date | ts: {{ dag_run.logical_date | ts }}"; \
-        echo "ts: {{ ts }}"; \
-        echo "ts_nodash_with_tz: {{ ts_nodash_with_tz }}"; \
-        echo "ts_nodash: {{ ts_nodash }}"; \
-        echo "prev_data_interval_start_success | ds: {{ prev_data_interval_start_success | ds }}"; \
-        echo "prev_data_interval_end_success | ds: {{ prev_data_interval_end_success | ds }}"; \
-        echo "prev_start_date_success | ds: {{ prev_start_date_success | ds }}"; \
-        echo "dag: {{ dag }}"; \
-        echo "task: {{ task }}"; \
-        echo "macros: {{ macros }}"; \
-        echo "task_instance: {{ task_instance }}";',
-    dag=dag
-)
+    show_template_variables = BashOperator(
+        task_id='show_template_variables',
+        bash_command='\
+            echo "data_interval_start | ds: {{ data_interval_start }}"; \
+            echo "data_interval_end | ds: {{ data_interval_end }}"; \
+            echo "dag_run.logical_date | ds: {{ dag_run.logical_date | ds }}"; \
+            echo "dag_run.logical_date | ds_nodash: {{ dag_run.logical_date | ds_nodash }}"; \
+            echo "dag_run.logical_date | ts: {{ dag_run.logical_date | ts }}"; \
+            echo "ts: {{ ts }}"; \
+            echo "ts_nodash_with_tz: {{ ts_nodash_with_tz }}"; \
+            echo "ts_nodash: {{ ts_nodash }}"; \
+            echo "prev_data_interval_start_success | ds: {{ prev_data_interval_start_success | ds }}"; \
+            echo "prev_data_interval_end_success | ds: {{ prev_data_interval_end_success | ds }}"; \
+            echo "prev_start_date_success | ds: {{ prev_start_date_success | ds }}"; \
+            echo "dag: {{ dag }}"; \
+            echo "task: {{ task }}"; \
+            echo "macros: {{ macros }}"; \
+            echo "task_instance: {{ task_instance }}";',
+        dag=dag
+    )
 
-t1
+    show_template_variables
