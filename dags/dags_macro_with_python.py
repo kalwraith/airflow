10effafbf1cc4087766aa411e09ca89a919f855e
hyunjinkim <kalwraith@naver.com>
2023-03-07 16:56:33 +0900

    코드 개선'

--- dags/dags_macro_with_python.py
+++ dags/dags_macro_with_python.py
@@ -1,6 +1,8 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.decorators import task
 import pendulum
+from dateutil.relativedelta import relativedelta
 
 with DAG(
     dag_id='dags_macro_with_python',
@@ -19,12 +21,31 @@ with DAG(
             print('templates_dict 파라미터가 없습니다')
 
     # 전월 1일 부터 말일까지 가져오기
-    task_1 = PythonOperator(
-        task_id='task_1',
+    task_using_macros = PythonOperator(
+        task_id='task_using_macros',
         templates_dict={'start_date':'{{ (data_interval_end + macros.dateutil.relativedelta.relativedelta(months=-1, day=1)) | ds }}',
                         'end_date': '{{ (data_interval_end - macros.dateutil.relativedelta.relativedelta(day=1) + macros.dateutil.relativedelta.relativedelta(days=-1)) | ds }}'
                         },
         python_callable=function_for_prev_month
     )
 
-    task_1
+
+    # 전월 1일부터 말일까지 python에서 직접 계산하기
+    @task(task_id='task_direct_calc')
+    def get_datetime_calc(**kwargs):
+        data_interval_start = kwargs['data_interval_start']
+        data_interval_end = kwargs['data_interval_end']
+        data_interval_start = data_interval_start + relativedelta(months=-1, day=1)
+        data_interval_end = data_interval_end + relativedelta(day=1) + relativedelta(days=-1)
+        print('data_interval_start:' + str(data_interval_start))
+        print('data_interval_end:' + str(data_interval_end))
+
+        data_interval_start_str = data_interval_start.strftime('%Y-%m-%d')
+        data_interval_end_str = data_interval_end.strftime('%Y-%m-%d')
+
+        print('data_interval_start_str:' + data_interval_start_str)
+        print('data_interval_end_str:' + data_interval_end_str)
+
+    task_direct_calc = get_datetime_calc()
+
+    task_using_macros >> task_direct_calc
