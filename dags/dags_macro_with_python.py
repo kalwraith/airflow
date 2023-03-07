173bb5cd444c3b8ca5f3a3eba2be8d3ca9bdfde8
hyunjinkim <kalwraith@naver.com>
2023-03-07 17:03:45 +0900

    데코레이터 수정

--- dags/dags_macro_with_python.py
+++ dags/dags_macro_with_python.py
@@ -11,7 +11,11 @@ with DAG(
     catchup=False
 ) as dag:
 
-    def function_for_prev_month(**kwargs):
+    @task(task_id='task_using_macros',
+          templates_dict={'start_date':'{{ (data_interval_end + macros.dateutil.relativedelta.relativedelta(months=-1, day=1)) | ds }}',
+                        'end_date': '{{ (data_interval_end - macros.dateutil.relativedelta.relativedelta(day=1) + macros.dateutil.relativedelta.relativedelta(days=-1)) | ds }}'
+                        })
+    def get_datetime_macro(**kwargs):
         template_dict = kwargs.get('templates_dict') or {}
         if template_dict:
             start_date = template_dict.get('start_date') or 'start_date없음'
@@ -20,15 +24,8 @@ with DAG(
         else:
             print('templates_dict 파라미터가 없습니다')
 
-    # 전월 1일 부터 말일까지 가져오기
-    task_using_macros = PythonOperator(
-        task_id='task_using_macros',
-        templates_dict={'start_date':'{{ (data_interval_end + macros.dateutil.relativedelta.relativedelta(months=-1, day=1)) | ds }}',
-                        'end_date': '{{ (data_interval_end - macros.dateutil.relativedelta.relativedelta(day=1) + macros.dateutil.relativedelta.relativedelta(days=-1)) | ds }}'
-                        },
-        python_callable=function_for_prev_month
-    )
 
+    task_using_macros = get_datetime_macro()
 
     # 전월 1일부터 말일까지 python에서 직접 계산하기
     @task(task_id='task_direct_calc')
