4fcf9c9bc5deec207ff0aa6e8a379fba4be4b3c5
hyunjinkim <kalwraith@naver.com>
2023-03-07 15:13:52 +0900

    task_decorator 사용법 간단히

--- dags/dags_task_decorator.py
+++ dags/dags_task_decorator.py
@@ -10,30 +10,22 @@ with DAG(
     catchup=False
 ) as dag:
 
-    @task
+    @task(task_id='py_task_1')
     def python_function1():
         tmp_lst = []
         for i in range(0,5):
             tmp_lst.append(i)
         return tmp_lst
 
-    task1 = python_function1()
+    py_task_1 = python_function1()
 
-    @task(task_id='python_task_2')
+    @task(task_id='py_task_2')
     def python_function2(lst: list):
         return {'list_content':[i + 100 for i in lst]}
 
 
-    @task
-    def python_function3(dict_v):
-        print(dict_v.get('list_content') or [])
+    py_task_2 = python_function2(py_task_1)
 
-    python_task_2 = python_function2(task1)
-    task3 = python_function3(python_task_2)
 
-    BashOperator(
-        task_id='bash_task',
-        bash_command="echo {{ ti.xcom_pull(task_ids='python_task_2', key='return_value') }}"
-    )
 
 
