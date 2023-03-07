47955bc257e495836710baec44e9f829909fb673
hyunjinkim <kalwraith@naver.com>
2023-03-07 16:08:24 +0900

    python 분할

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -3,23 +3,13 @@ from datetime import datetime
 from airflow.operators.python import PythonOperator
 
 
-def python_function1(*args):
-    print(type(args))
-    print(args)
+def python_function1():
+    sample_list = [1,2,3,4,5]
+    return_lst = []
+    for i in sample_list:
+        return_lst.append(i)
 
-
-def python_function2(**kwargs):
-    name = kwargs.get('name') or ''
-    address = kwargs.get('address') or ''
-    age = kwargs.get('age') or ''
-    print('name:' + name)
-    print('address:' + address)
-    print('age:' + age)
-
-def python_function3(*args, **kwargs):
-    print(args)
-    print('ti:' + str(kwargs['ti']))
-    print('dag_run:' + str(kwargs['dag_run']))
+    print(return_lst)
 
 
 with DAG(
@@ -30,23 +20,7 @@ with DAG(
 
     python_task_1 = PythonOperator(
         task_id='python_task_1',
-        python_callable=python_function1,
-        op_args=['We','are','studying','airflow']
-
+        python_callable=python_function1
     )
 
-    python_task_2 = PythonOperator(
-        task_id='python_task_2',
-        python_callable=python_function2,
-        op_kwargs={'name':'hjkim','address':'seoul'}
-    )
-
-    python_task_3 = PythonOperator(
-        task_id='python_task_3',
-        python_callable=python_function3,
-        op_args=['python','operator'],
-        op_kwargs={'name':'hjkim','address':'seoul'}
-    )
-
-
-    python_task_1 >> python_task_2 >> python_task_3
+    python_task_1
