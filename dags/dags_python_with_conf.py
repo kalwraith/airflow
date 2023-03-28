bb74c19b4b22ee2b05e0c324015b972772668d84
hyunjinkim <kalwraith@naver.com>
2023-03-28 15:08:55 +0900

    dag_run 업로드

---
+++ dags/dags_python_with_conf.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.decorators import task
+import pendulum
+
+with DAG(
+    dag_id='dags_python_with_conf',
+    start_date=pendulum.datetime(2023,3,20, tz='Asia/Seoul'),
+    schedule='2 0 * * *'
+) as dag:
+
+    @task(task_id='task_sample')
+    def task_sample(**kwargs):
+        dag_run = kwargs['dag_run']
+        print(dag_run.conf)
+
+    task_sample()
