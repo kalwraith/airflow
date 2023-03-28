0c416401379e44e8ff42574a81ffd545a0121b68
hyunjinkim <kalwraith@naver.com>
2023-03-28 13:35:48 +0900

    new dag ㅇㅂ로드

---
+++ dags/dags_bash_with_xcom.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+import pendulum
+
+with DAG(
+    dag_id='dags_bash_with_xcom',
+    start_date=pendulum.datetime(2023,2,1, tz='Asia/Seoul'),
+    schedule=None,
+    catchup=False
+) as dag:
+    bash_push = BashOperator(
+        task_id='bash_push',
+        bash_command="{{ ti.xcom_push(key='bash_pushed',value='first_bash_message') }} && "
+                     "echo second_bash_message"
+    )
+
+    bash_pull = BashOperator(
+        task_id='bash_pull',
+        env={'FIRST_VALUE': "{{ ti.xcom_push(key='bash_pushed') }}",
+             'SECOND_VALUE': "{{ ti.xcom_push(key='return_value', task_ids='bash_push') }}"},
+        bash_command="echo $FIRST_VALUE && echo $SECOND_VALUE "
+    )
