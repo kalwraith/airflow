4c963187fb3492364d1ef24def38621d2327fc4a
hyunjinkim <kalwraith@naver.com>
2023-03-29 17:43:09 +0900

    bash with vars 수정

--- dags/dags_bash_with_vars.py
+++ dags/dags_bash_with_vars.py
@@ -1,7 +1,10 @@
 # Package Import
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.models import Variable
 import pendulum
+
+value = Variable.get('sample')
 
 # plugins 폴더 공유를 통해 컨테이너 외부에 있는 shell 파일 수행하기
 with DAG(
@@ -10,9 +13,14 @@ with DAG(
     catchup=False,
     schedule='0 1 * * *'
 ) as dag:
-    bash_task = BashOperator(
-        task_id='bash_task',
+    bash_bad_vars = BashOperator(
+        task_id='bash_bad_vars',
+        bash_command=f'echo "variable:{value}"'
+    )
+
+    bash_good_vars = BashOperator(
+        task_id='bash_good_vars',
         bash_command='echo "{{var.value.sample}}"'
     )
 
-    bash_task
+    bash_bad_vars >> bash_good_vars
