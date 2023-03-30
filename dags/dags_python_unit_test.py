29ec84496987246814338e4daee69f24d821222b
hyunjinkim <kalwraith@naver.com>
2023-03-30 17:05:31 +0900

    수정

--- dags/dags_python_unit_test.py
+++ dags/dags_python_unit_test.py
@@ -9,7 +9,10 @@ with DAG(
     dag_id='dags_python_unit_test',
     start_date=pendulum.datetime(2023,2,20, tz='Asia/Seoul'),
     schedule='0 1 * * *',
-    catchup=False
+    catchup=False,
+    params={
+        'flow1_table':'TBL_TEST'
+    }
 ) as dag:
     task_sample_python_flow1 = sample_python_flow1.flow()
     task_sample_python_flow2 = sample_python_flow2.flow()
--- plugins/flows/sample_python_flow1.py
+++ plugins/flows/sample_python_flow1.py
@@ -1,17 +1,21 @@
 from airflow import DAG
 from datetime import datetime
 from airflow.operators.python import PythonOperator
+from operators.make_fin_operator import MakeFinOperator
 import pendulum
 
 def flow():
-
-    def test():
-        print('sample python flow1')
 
     t1 = PythonOperator(
         task_id='sample_python_flow1',
         python_callable=test
     )
 
-    return t1
+    fin_task = MakeFinOperator(
+        task_id='sample_make_fin',
+        path='l0/cm/{{params.flow1_table }}/{{ data_interval_end.in_timezone("Asia/Seoul").year }}/{{ data_interval_end.in_timezone("Asia/Seoul").month }}/{{ data_interval_end.in_timezone("Asia/Seoul").day }}',
+        file_name='fin'
+    )
+
+    return t1 >> fin_task
 
