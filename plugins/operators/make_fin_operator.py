f2d67317174f0307d3d925de97b0e740a15ed35a
hyunjinkim <kalwraith@naver.com>
2023-03-30 16:44:27 +0900

    Context 제거

--- plugins/operators/make_fin_operator.py
+++ plugins/operators/make_fin_operator.py
@@ -9,7 +9,7 @@ class MakeFinOperator(BaseOperator):
         self.path = path[1:] if path.startswith('/') else path
         self.file_name = file_name
 
-    def execute(self, context: Context):
+    def execute(self, context):
         base_dir = '/opt/airflow/files/fin'
         tgt_dir = base_dir + '/' + self.path
         Path(tgt_dir).mkdir(parents=True, exist_ok=True)
