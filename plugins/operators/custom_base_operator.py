01c6f17437ec49ab5d8f22372c6a89fbc65e06fe
hyunjinkim <kalwraith@naver.com>
2023-03-30 18:04:31 +0900

    수정

--- plugins/operators/custom_base_operator.py
+++ plugins/operators/custom_base_operator.py
@@ -1,17 +1,20 @@
 from airflow.models.baseoperator import BaseOperator
 from common import substitute_parameters_with_context
+from airflow.utils.log.logging_mixin import LoggingMixin
+
+
 class CustomBaseOperator(BaseOperator):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         from pprint import pprint
-        pprint(kwargs)
+        LoggingMixin().log.info(kwargs)
         #self.data_interval_end = kwargs['data_interval_end']
 
     def execute(self, context):
-
-
-
+        '''
+        로깅 등 공통로직 처리
+        '''
 
         self._execute()
 
