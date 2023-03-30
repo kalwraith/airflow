5d51199dfc6a768259dfbf795cffdddf5af52566
hyunjinkim <kalwraith@naver.com>
2023-03-30 16:27:19 +0900

    operator 수정

--- plugins/operators/make_fin_operator.py
+++ plugins/operators/make_fin_operator.py
@@ -1,4 +1,5 @@
 from airflow.models.baseoperator import BaseOperator
+from pathlib import Path
 
 class MakeFinOperator(BaseOperator):
     template_fields = ['path', 'file_name']
@@ -10,5 +11,8 @@ class MakeFinOperator(BaseOperator):
 
     def execute(self, context: Context):
         base_dir = '/opt/airflow/files/fin'
+        tgt_dir = base_dir + '/' + self.path
+        Path(tgt_dir).mkdir(parents=True, exist_ok=True)
+
         f = open(f'{base_dir}/{self.path}/{self.file_name}', 'w')
         f.close()
