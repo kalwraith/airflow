feef7ac9c84b17590b1576a1beabd46c9c1f2626
hyunjinkim <kalwraith@naver.com>
2023-03-31 18:00:39 +0900

    수정

--- plugins/operators/make_fin_operator.py
+++ plugins/operators/make_fin_operator.py
@@ -7,9 +7,9 @@ class MakeFinOperator(CustomBaseOperator):
     def __init__(self, path, file_name, **kwargs):
         task_meta = kwargs.get('task_meta')
         if task_meta:
-            task_id = kwargs.get('task_id')
-        else:
             task_id = get_task_id(task_meta=task_meta, task_nm='make_fin')
+        else:
+            task_id = kwargs.get('task_id')
         super().__init__(task_id=task_id, **kwargs)
         self.path = path[1:] if path.startswith('/') else path
         self.file_name = file_name
