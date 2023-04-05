9439c3fe42e347f9b07a20b8f3a5bd8636328f22
hyunjinkim <kalwraith@naver.com>
2023-04-05 12:23:01 +0900

    세부 수정

--- dags/dags_bash_python_with_xcom.py
+++ dags/dags_bash_python_with_xcom.py
@@ -11,17 +11,17 @@ with DAG(
     catchup=False
 ) as dag:
 
-    @task(task_id='python_result')
-    def push_xcom():
+    @task(task_id='python_push')
+    def python_push_xcom():
         result_dict = {'status':'Good','data':[1,2,3],'options_cnt':100}
         return result_dict
 
     bash_pull = BashOperator(
-        task_id='bask_pull',
+        task_id='bash_pull',
         env={
-            'STATUS':'{{ti.xcom_pull(task_ids="python_result")["status"]}}',
-            'DATA':'{{ti.xcom_pull(task_ids="python_result")["data"]}}',
-            'OPTIONS_CNT':'{{ti.xcom_pull(task_ids="python_result")["options_cnt"]}}'
+            'STATUS':'{{ti.xcom_pull(task_ids="python_push")["status"]}}',
+            'DATA':'{{ti.xcom_pull(task_ids="python_push")["data"]}}',
+            'OPTIONS_CNT':'{{ti.xcom_pull(task_ids="python_push")["options_cnt"]}}'
 
         },
         bash_command='echo $STATUS && echo $DATA && echo $OPTIONS_CNT'
@@ -35,13 +35,13 @@ with DAG(
     )
 
     @task(task_id='python_pull')
-    def pulling(**kwargs):
+    def python_pull_xcom(**kwargs):
         ti = kwargs['ti']
         status_value = ti.xcom_pull(key='bash_pushed')
         return_value = ti.xcom_pull(task_ids='bash_push')
         print(status_value)
         print(return_value)
 
-    push_xcom() >> bash_pull
+    python_push_xcom() >> bash_pull
 
-    bash_push >> pulling()
+    bash_push >> python_pull_xcom()
