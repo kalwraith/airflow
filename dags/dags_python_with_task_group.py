1eac7fe4811270ec7a3a758183c50ab8c7761ce2
hyunjinkim <kalwraith@naver.com>
2023-04-07 10:02:41 +0900

    task group 수정

--- dags/dags_python_with_task_group.py
+++ dags/dags_python_with_task_group.py
@@ -3,6 +3,7 @@ from airflow.decorators import task
 from airflow.decorators import task_group
 from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
+from airflow.utils.task_group import TaskGroup
 import pendulum
 
 with DAG(
@@ -11,26 +12,40 @@ with DAG(
     schedule='0 1 * * *',
     catchup=False
 ) as dag:
+    def inner_func2(**kwargs):
+        msg = kwargs.get('msg') or ''
+        print(msg)
+
     empty_start = EmptyOperator(task_id='empty_start')
 
     @task_group(group_id='first_group')
     def group_1():
-        ''' 첫 번째 그룹에 대한 설명입니다. '''
+        ''' task_group 데커레이터를 이용한 첫 번째 그룹입니다. '''
+
         @task(task_id='inner_fuction1')
         def inner_func1(**kwargs):
             print('첫 번째 함수입니다.')
 
-        def inner_func2():
-            print('두 번째 함수입니다.')
-
         inner_function2 = PythonOperator(
             task_id='inner_function2',
-            python_callable=inner_func2
+            python_callable=msg,
+            op_kwargs={'msg':'첫 번째 TaskGroup내 두 번쨰 task입니다.'}
         )
 
         inner_func1() >> inner_function2
 
+    with TaskGroup(group_id='second_group') as group_2:
+        @task(task_id='inner_fuction1')
+        def inner_func3(**kwargs):
+            print('세 번째 함수입니다.')
+
+
+        inner_function2 = PythonOperator(
+            task_id='inner_function2',
+            python_callable=msg,
+            op_kwargs={'msg': '두 번째 TaskGroup내 두 번쨰 task입니다.'}
+        )
 
     empty_final = EmptyOperator(task_id='empty_final')
 
-    empty_start >> group_1()>> empty_final
+    empty_start >> [group_1(), group_2] >> empty_final
