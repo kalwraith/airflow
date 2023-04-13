997aff0ed521be9eb60d540134d1fa18e8162c79
hyunjinkim <kalwraith@naver.com>
2023-04-13 12:53:36 +0900

    template 컬럼 추가

--- plugins/operators/seoul_api_to_csv_operator.py
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -5,13 +5,13 @@ import pandas as pd
 class SeoulApiToCsvOperator(BaseOperator):
     template_fields = ('endpoint', 'path','file_name','date_dt')
 
-    def __init__(self, dataset_nm, path, file_name, date_dt=None, **kwargs):
+    def __init__(self, dataset_nm, path, file_name, base_dt=None, **kwargs):
         super().__init__(**kwargs)
         self.http_conn_id = 'openapi.seoul.go.kr'
         self.path = path
         self.file_name = file_name
         self.endpoint = '{{var.value.apikey_openapi_seoul_go_kr}}/json/' + dataset_nm
-        self.date_dt = date_dt
+        self.base_dt = base_dt
 
     def execute(self, context):
         import os
