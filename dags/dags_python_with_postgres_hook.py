8d271f46742fecc8402b701ea09e2e8198817929
hyunjinkim <kalwraith@naver.com>
2023-04-20 14:21:00 +0900

    postgres hook

--- dags/dags_python_with_postgres_hook.py
+++ dags/dags_python_with_postgres_hook.py
@@ -2,33 +2,32 @@ from airflow import DAG
 import pendulum
 from airflow.decorators import task
 from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 with DAG(
-    dag_id='dags_python_with_postgres',
-    start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'),
-    schedule=None,
-    catchup=False
+        dag_id='dags_python_with_postgres_hook',
+        start_date=pendulum.datetime(2023, 4, 1, tz='Asia/Seoul'),
+        schedule=None,
+        catchup=False
 ) as dag:
-
-    
-    def insrt_postgres(ip, port, dbname, user, passwd, **kwargs):
-        import psycopg2
-
-        conn = psycopg2.connect(host=ip, dbname=dbname, user=user, password=passwd, port=int(port))
+    def insrt_postgres(postgres_conn_id, **kwargs):
+        postgres_hook = PostgresHook(postgres_conn_id)
+        conn = postgres_hook.get_connection()
         cursor = conn.cursor()
         dag_id = kwargs.get('ti').dag_id
         task_id = kwargs.get('ti').task_id
         run_id = kwargs.get('ti').run_id
         sql = 'insert into test_python_operator values (%s,%s,%s,%s);'
         msg = 'insrt 수행'
-        cursor.execute(sql,(dag_id,task_id,run_id,msg))
+        cursor.execute(sql, (dag_id, task_id, run_id, msg))
         conn.commit()
         conn.close()
-    
+
+
     insrt_postgres = PythonOperator(
         task_id='insrt_postgres',
         python_callable=insrt_postgres,
-        op_args=['172.18.0.3', '5432', 'hjkim', 'hjkim', 'hjkim']
+        op_kwargs={'postgres_conn_id':'conn-db-postgres-custom'}
     )
-        
+
     insrt_postgres
