f5f53d226a5feb188dcbfb02fb35a05086e17425
hyunjinkim <kalwraith@naver.com>
2023-04-21 10:39:05 +0900

    bulk_load 작성

---
+++ dags/dags_python_with_postgres_hook_bulk_load.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+import pendulum
+from airflow.decorators import task
+from airflow.operators.python import PythonOperator
+
+with DAG(
+        dag_id='dags_python_with_postgres_hook',
+        start_date=pendulum.datetime(2023, 4, 1, tz='Asia/Seoul'),
+        schedule='0 7 * * *',
+        catchup=False
+) as dag:
+    def insrt_postgres(postgres_conn_id, **kwargs):
+        from airflow.providers.postgres.hooks.postgres import PostgresHook
+        from contextlib import closing
+
+        postgres_hook = PostgresHook(postgres_conn_id)
+        postgres_hook.bulk_load('TbCorona19CountStatus',
+                                '/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}/TbCorona19CountStatus.csv')
+
+    insrt_postgres = PythonOperator(
+        task_id='insrt_postgres',
+        python_callable=insrt_postgres,
+        op_kwargs={'postgres_conn_id': 'conn-db-postgres-custom'}
+    )
