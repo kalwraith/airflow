f5fb3d7927625797b73280984f7e7c90ff5bf196
hyunjinkim <kalwraith@naver.com>
2023-04-21 15:24:49 +0900

    수정

--- plugins/hooks/custom_postgres_hook.py
+++ plugins/hooks/custom_postgres_hook.py
@@ -32,7 +32,7 @@ class CustomPostgresHook(BaseHook):
         for col in file_df.columns:                             # 줄넘김 및 ^M 제거
             try:
                 # string 문자열이 아닐 경우 continue
-                file_df[col].str.replace('\r','').replace('\n','', inplace=True)
+                file_df[col] = file_df[col].str.replace('\r','').replace('\n','')
             except:
                 continue 
                 
