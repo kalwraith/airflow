ba292707c875859b004cadaf01f0cf6c6923f8b0
hyunjinkim <kalwraith@naver.com>
2023-04-21 17:30:16 +0900

    bash 센서 등록

---
+++ dags/dags_bash_sensor.py
@@ -0,0 +1,31 @@
+from airflow.sensors.bash import BashSensor
+from airflow.operators.bash import BashOperator
+from airflow import DAG
+import pendulum
+
+with DAG(
+    dag_id='dags_bash_sensor',
+    start_date=pendulum.datetime(2023,3,1, tz='Asia/Seoul'),
+    schedule='0 6 * * *',
+    catchup=False
+) as dag:
+
+    sensor_task = BashSensor(
+        task_id='sensor_task',
+        env={'FILE':'/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}/tvCorona19VaccinestatNew.csv'},
+        bash_command='if [ -e $FILE ]; then \
+                          exit 0 \
+                      else \
+                          exit 1 \
+                      fi',
+        poke_interval=600,    # 10분
+        timeout=60 * 60 * 24  # 1일
+    )
+
+    bash_task = BashOperator(
+        task_id='bash_task',
+        env={'FILE': '/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}/tvCorona19VaccinestatNew.csv'},
+        bash_command='echo $FILE'
+    )
+
+    sensor_task >> bash_task
