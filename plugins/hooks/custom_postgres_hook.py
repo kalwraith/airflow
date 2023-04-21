045ad29eb95a6afa9ab9d3a9e463deb8e5f4fbd2
hyunjinkim <kalwraith@naver.com>
2023-04-21 14:28:53 +0900

    replace_yn 추가

--- plugins/hooks/custom_postgres_hook.py
+++ plugins/hooks/custom_postgres_hook.py
@@ -20,11 +20,12 @@ class CustomPostgresHook(BaseHook):
         self.postgres_conn = psycopg2.connect(host=self.host, user=self.user, password=self.password, dbname=self.dbname, port=self.port)
 
 
-    def bulk_load(self, table_name, file_name, delimiter: str, header_yn: bool):
+    def bulk_load(self, table_name, file_name, delimiter: str, header_yn: bool, replace_yn: bool):
         from sqlalchemy import create_engine
         
         self.get_conn()
-        header = 0 if header_yn else None               # header_yn = True면 0, False면 None
+        header = 0 if header_yn else None                       # header_yn = True면 0, False면 None
+        if_exists = 'replace' if replace_yn else 'append'       # replace_yn = True면 replace, False면 append
         file_df = pd.read_csv(file_name, header=header, delimiter=delimiter)
         file_df = file_df.apply(lambda x: x.replace('\r',''))       # 개행문자 ^M 제거 
         uri = f'postgresql://{self.user}:{self.password}@{self.host}/{self.dbname}'
@@ -32,6 +33,6 @@ class CustomPostgresHook(BaseHook):
         file_df.to_sql(name=table_name,
                             con=engine,
                             schema='public',
-                            if_exists='insert',
+                            if_exists=if_exists,
                             index=False
                         )
