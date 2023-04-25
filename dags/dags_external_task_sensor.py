907a2d4fbb7a7f9987bd2e62c3e1429e7cb2bf65
hyunjinkim <kalwraith@naver.com>
2023-04-25 17:31:15 +0900

    수정' git push

--- dags/dags_external_task_sensor.py
+++ dags/dags_external_task_sensor.py
@@ -3,6 +3,8 @@ from airflow.sensors.external_task import ExternalTaskSensor
 from airflow.operators.python import PythonOperator
 import pendulum
 from datetime import timedelta
+from airflow.utils.state import State
+
 with DAG(
     dag_id='dags_external_task_sensor',
     start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'),
@@ -13,6 +15,6 @@ with DAG(
         task_id='external_task_sensor',
         external_dag_id='dags_branch_python_operator',
         external_task_id='task_b',
-        allowed_states='skipped',
+        allowed_states=[State.SKIPPED],
         execution_delta=timedelta(hours=-6)
     )
