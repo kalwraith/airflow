56c2e53dd84e996f2f7dc71056ccc47da3e7c4d7
hyunjinkim <kalwraith@naver.com>
2023-04-26 13:46:29 +0900

    closing 추가

--- plugins/sensors/postgres_table_sensor.py
+++ plugins/sensors/postgres_table_sensor.py
@@ -1,6 +1,6 @@
 from airflow.sensors.base import BaseSensorOperator
 from hooks.custom_postgres_hook import CustomPostgresHook
-
+from contextlib import closing
 class PostgresTableSensor(BaseSensorOperator):
     template_fields = ('table_name')
     def __init__(self, postgres_conn_id, table_name, **kwargs):
@@ -11,17 +11,18 @@ class PostgresTableSensor(BaseSensorOperator):
 
     def poke(self, context):
         custom_postgres_hook = CustomPostgresHook(self.postgres_conn_id)
-        postgres_conn = custom_postgres_hook.get_conn()
-        cur = postgres_conn.cursor()
-        check_sql = f"select count(1) from pg_tables where schemaname='public' AND tablename= %s ;"
-        self.log.info(f'조회 쿼리: {check_sql}')
-
-        cur.execute(check_sql, (self.table_name,))
-        rslt_set = cur.fetchall()
-        rslt = int(rslt_set[0][0])
-        self.log.info(f'테이블 개수: {rslt}')
+        with closing(custom_postgres_hook.get_conn()) as postgres_conn:
+            with closing(postgres_conn.cursor()) as cur:
+                check_sql = f"select count(1) from pg_tables where schemaname='public' AND tablename= %s ;"
+                self.log.info(f'조회 쿼리: {check_sql}')
+                self.log.info(f'테이블명:{self.table_name}')
         
-        if rslt > 0:
-            return True 
-        else:
-            return False
+                cur.execute(check_sql, (self.table_name,))
+                rslt_set = cur.fetchall()
+                rslt = int(rslt_set[0][0])
+                self.log.info(f'테이블 개수: {rslt}')
+                
+                if rslt > 0:
+                    return True 
+                else:
+                    return False
