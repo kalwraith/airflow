a2858044a72d5cd92e22ba500ee9bfaef18078c9
hyunjinkim <kalwraith@naver.com>
2023-04-27 08:53:38 +0900

    dag 등록

---
+++ dags/dags_transport_api_to_postgres.py
@@ -0,0 +1,19 @@
+from operators.transport_api_to_postgres_operator import TransportApiToPostgresOperator
+from airflow import DAG
+import pendulum
+
+with DAG(
+        dag_id='dags_transport_api_to_postgres',
+        schedule=None,
+        start_date=pendulum.datetime(2023, 4, 1, tz='Asia/Seoul'),
+        catchup=False
+) as dag:
+    #실시간 영업소간 통행시간
+    real_unit_trtm_vo = SeoulApiToPostgresOperator(
+        task_id='real_unit_trtm_vo',
+        product_id='PRDTNUM_000000020313',
+        tgt_tbl_nm='real_unit_trtm_vo',
+        option_dict={'sumTmUnitTypeCode:3'}
+    )
+
+
