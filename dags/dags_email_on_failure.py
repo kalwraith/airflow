41b118720df3943d77a01e247a1f4172d91a13b5
hyunjinkim <kalwraith@naver.com>
2023-05-03 13:26:55 +0900

    email_on_faulure 추가

---
+++ dags/dags_email_on_failure.py
@@ -0,0 +1,25 @@
+# Package Import
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+import pendulum
+
+with DAG(
+    dag_id='dags_bash_operator',
+    start_date=pendulum.datetime(2023,2,16, tz='Asia/Seoul'),
+    catchup=False,
+    schedule='0 1 * * *',
+    default_args={
+        'email_on_failure': True
+    }
+) as dag:
+    bash_task_1 = BashOperator(
+        task_id='bash_task_1',
+        bash_command='exit 1',
+    )
+    bash_task_2 = BashOperator(
+        task_id='bash_task_2',
+        bash_command='exit 0',
+    )
+
+
+
