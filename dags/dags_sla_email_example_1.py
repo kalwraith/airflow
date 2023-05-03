abf629e4d5a605fa9a553887039929b8cacfbd62
hyunjinkim <kalwraith@naver.com>
2023-05-03 15:19:14 +0900

    수정' git push

--- dags/dags_sla_email_example_1.py
+++ dags/dags_sla_email_example_1.py
@@ -5,22 +5,23 @@ import pendulum
 
 with DAG(
     dag_id='dags_sla_email_example_1',
-        start_date=pendulum.datetime(2023, 4, 1, tz='Asia/Seoul'),
-        schedule='*/30 * * * *',
-        catchup=False
+    start_date=pendulum.datetime(2023, 4, 1, tz='Asia/Seoul'),
+    schedule='*/30 * * * *',
+    catchup=False,
+    default_args={
+        'sla':timedelta(minutes=5),
+        'email':'hjkim_sun@naver.com, kalwraith@gamil.com'
+    }
 ) as dag:
     
     task1 = BashOperator(
         task_id='task1',
         bash_command='sleep 10m',
-        sla=timedelta(minutes=5), # SLA 설정
-        email='hjkim_sun@naver.com'
     )
     
     task2 = BashOperator(
         task_id='task2',
         bash_command='sleep 2m',
-        sla=timedelta(minutes=1), # SLA 설정
-        email='hjkim_sun@naver.com'
+        sla=timedelta(minutes=1)
     )
     
