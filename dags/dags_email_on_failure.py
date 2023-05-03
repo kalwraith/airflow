d6ad7cfe8d5678534dd0be35b3770bd904b3fc85
hyunjinkim <kalwraith@naver.com>
2023-05-03 16:43:41 +0900

    dag 등록

--- dags/dags_email_on_failure.py
+++ dags/dags_email_on_failure.py
@@ -2,24 +2,30 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 import pendulum
+from datetime import timedelta
+from airflow.models import Variable
+
+email_str = Variable.get("email_target")
+email_lst = email_str.split(',')
 
 with DAG(
     dag_id='dags_email_on_failure',
-    start_date=pendulum.datetime(2023,2,16, tz='Asia/Seoul'),
+    start_date=pendulum.datetime(2023,4,16, tz='Asia/Seoul'),
     catchup=False,
     schedule='0 1 * * *',
+    dagrun_timeout=timedelta(minutes=2),
     default_args={
         'email_on_failure': True,
-        'email': '{{var.value.email_target}}'
+        'email': email_lst
     }
 ) as dag:
-    bash_task_1 = BashOperator(
-        task_id='bash_task_1',
-        bash_command='exit 1',
+    task_sleep_180 = BashOperator(
+        task_id='task_sleep_180',
+        bash_command='sleep 180',
     )
-    bash_task_2 = BashOperator(
-        task_id='bash_task_2',
-        bash_command='exit 0',
+    task_sleep_60 = BashOperator(
+        task_id='task_sleep_60',
+        bash_command='sleep 60',
     )
 
 
