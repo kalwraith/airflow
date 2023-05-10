8adfa8560afd219fcaee987b4e53b613c4d0e5c7
hyunjinkim <kalwraith@naver.com>
2023-05-11 08:51:34 +0900

    수정' git

--- dags/dags_sla_miss_callback_to_kakao.py
+++ dags/dags_sla_miss_callback_to_kakao.py
@@ -8,32 +8,32 @@ from config.sla_miss_callback_func import sla_miss_callback_to_kakao
 with DAG(
     dag_id='dags_sla_miss_callback_to_kakao',
     start_date=pendulum.datetime(2023, 5, 1, tz='Asia/Seoul'),
-    schedule='*/10 * * * *',
+    schedule='*/20 * * * *',
     catchup=False,
     sla_miss_callback=sla_miss_callback_to_kakao
 ) as dag:
     task_sla_30s = BashOperator(
         task_id='task_sla_30s',
-        bash_command='sleep 35',
-        sla=timedelta(seconds=30)
+        bash_command='sleep 100',
+        sla=timedelta(minutes=2)
     )
 
     task_sla_40s = BashOperator(
         task_id='task_sla_40s',
-        bash_command='sleep 25',
-        sla=timedelta(seconds=40)
+        bash_command='sleep 100',
+        sla=timedelta(minutes=3)
     )
 
     task_sla_50s = BashOperator(
         task_id='task_sla_50s',
-        bash_command='sleep 25',
-        sla=timedelta(seconds=50)
+        bash_command='sleep 60',
+        sla=timedelta(minutes=4)
     )
 
     task_sla_60s = BashOperator(
         task_id='task_sla_60s',
-        bash_command='sleep 25',
-        sla=timedelta(seconds=70)
+        bash_command='sleep 60',
+        sla=timedelta(minutes=5)
     )
 
 
