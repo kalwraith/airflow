87619c8ea38d6a82b91e934203df0ef2e7a24f9f
hyunjinkim <kalwraith@naver.com>
2023-05-10 14:01:12 +0900

    sla_miss_callback 테스트

---
+++ dags/dags_sla_miss_callback_to_kakao.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import timedelta
+import pendulum
+from config.sla_miss_callback_func import sla_miss_callback_to_kakao
+
+
+with DAG(
+    dag_id='dags_sla_miss_callback_to_kakao',
+    start_date=pendulum.datetime(2023, 5, 1, tz='Asia/Seoul'),
+    schedule='*/30 * * * *',
+    catchup=False,
+    default_args={
+        'sla': timedelta(seconds=20),
+        'email': email_lst
+    },
+    sla_miss_callback=sla_miss_callback_to_kakao
+) as dag:
+    task_sla_25s = BashOperator(
+        task_id='task_sla_25s',
+        bash_command='sleep 25',
+    )
+
+    task_sla_30s = BashOperator(
+        task_id='task_sla_30s',
+        bash_command='sleep 30',
+    )
+
---
+++ plugins/config/sla_miss_callback_func.py
@@ -0,0 +1,31 @@
+from kakao_api import send_kakao_msg
+from airflow.models import Variable
+def sla_miss_callback_to_kakao(dag, task_list, blocking_task_list, slas, blocking_tis):
+    '''
+    :param dag:
+    :param task_list:
+    :param blocking_task_list:
+    :param slas:
+    :param blocking_tis:
+    :return:
+    '''
+    print(
+        "The callback arguments are: ",
+        {
+            "dag": dag,
+            "task_list": task_list,
+            "blocking_task_list": blocking_task_list,
+            "slas": slas,
+            "blocking_tis": blocking_tis,
+        },
+    )
+    client_id=Variable.get("kakao_client_secret")
+    content = []
+    for task in task_list:
+        content.append({
+            task: f'sla Miss 발생({slas})'
+        })
+
+    send_kakao_msg(client_id=client_id,
+                   talk_title=f'{dag} 수행 결과',
+                   content=content)
