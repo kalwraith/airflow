00277c5e423a97201ff4c830a0eaabde275876fa
hyunjinkim <kalwraith@naver.com>
2023-05-10 16:12:54 +0900

    수정'

--- plugins/config/sla_miss_callback_func.py
+++ plugins/config/sla_miss_callback_func.py
@@ -2,8 +2,8 @@ from config.kakao_api import send_kakao_msg
 from airflow.models import Variable
 def sla_miss_callback_to_kakao(dag, task_list, blocking_task_list, slas, blocking_tis):
     '''
-    :param dag:
-    :param task_list: delimiter(\n)로 구분된 list, (Ex: task_1 on 2023-05-10T006:00:00+00:00\ntask_2 on 2023-05-10T006:00:00+00:00)
+    :param dag: DAG class 객체
+    :param task_list: delimiter(\n)로 구분된 string, (Ex: task_1 on 2023-05-10T006:00:00+00:00\ntask_2 on 2023-05-10T006:00:00+00:00)
     :param blocking_task_list:
     :param slas:
     :param blocking_tis:
@@ -23,7 +23,7 @@ def sla_miss_callback_to_kakao(dag, task_list, blocking_task_list, slas, blockin
     #slas_lst = [i for i in slas[0]]
 
     content = {}
-    content['1'] = str(type(slas))
+    content['1'] = slas
     #content['3'] = slas_lst[4]
     # for task in task_list.split('\n'):
     #     content[task] = str(slas_lst[2])
