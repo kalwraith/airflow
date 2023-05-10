158a359548ce72d79074acc1b36c0b1666a57c67
hyunjinkim <kalwraith@naver.com>
2023-05-10 09:15:19 +0900

    dag 업로드

---
+++ dags/dags_kakao_test.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.python import PythonOperator
+from config.kakao_api import send_kakao_msg
+import pendulum
+
+with DAG(
+    dag_id='dags_kakao_test',
+    start_date=pendulum.datetime(2023,5,1, tz='Asia/Seoul'),
+    schedule=None,
+    catchup=False
+) as dag:
+
+    python_kakao_test = PythonOperator(
+        task_id='python_kakao_test',
+        python_callable=send_kakao_msg
+    )
+
