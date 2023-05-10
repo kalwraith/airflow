2da9b83d94176153f14021d82fc4f411edf25d52
hyunjinkim <kalwraith@naver.com>
2023-05-10 16:17:49 +0900

    수정'

--- plugins/config/sla_miss_callback_func.py
+++ plugins/config/sla_miss_callback_func.py
@@ -22,7 +22,7 @@ def sla_miss_callback_to_kakao(dag, task_list, blocking_task_list, slas, blockin
     client_id = Variable.get("kakao_client_secret")
     content = {}
     for task in task_list.split('\n'):
-        content[task] = 'sla 실패'
+        content[task] = str(type(slas))
 
     send_kakao_msg(client_id=client_id,
                    talk_title=f'{dag.dag_id} SLA Miss 발생',
