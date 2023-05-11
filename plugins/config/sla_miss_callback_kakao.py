312bd81b51264e002b04b0cee9cd3e9df9cff6fe
hyunjinkim <kalwraith@naver.com>
2023-05-11 13:09:40 +0900

    주석 추가

--- plugins/config/sla_miss_callback_kakao.py
+++ plugins/config/sla_miss_callback_kakao.py
@@ -4,23 +4,15 @@ import pendulum
 
 def sla_miss_callback_to_kakao(dag, task_list, blocking_task_list, slas, blocking_tis):
     '''
+    sla_miss_callback의 경우 print 하는 내용이 airflow Task Log에 남지 않으며 디버깅의 어려움이 존재함
+
     :param dag: DAG class 객체
     :param task_list: delimiter(\n)로 구분된 string, (Ex: task_1 on 2023-05-10T06:00:00+00:00\ntask_2 on 2023-05-10T006:00:00+00:00)
     :param blocking_task_list: Empty list
     :param slas: list로 감싸여진 slaMiss 객체
     :param blocking_tis: Empty list
-    :return:
     '''
-    print(
-        "The callback arguments are: ",
-        {
-            "dag": dag,
-            "task_list": task_list,
-            "blocking_task_list": blocking_task_list,
-            "slas": slas,
-            "blocking_tis": blocking_tis,
-        },
-    )
+
     client_id = Variable.get("kakao_client_secret")
     content = {}
     for task in task_list.split('\n'):
