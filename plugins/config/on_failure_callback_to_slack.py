bfbb5cb9a4803d29e3519b90f8f4b44acb1d2b6b
hyunjinkim <kalwraith@naver.com>
2023-05-12 14:00:34 +0900

    업로드

--- plugins/config/on_failure_callback_to_slack.py
+++ plugins/config/on_failure_callback_to_slack.py
@@ -5,7 +5,7 @@ def on_failure_callback_to_slack(context):
     dag_id = ti.dag_id
     task_id = ti.task_id
     err_msg = context.get('exception')
-    #batch_date = context('data_interval_end').in_timezone('Asia/Seoul')
+    batch_date = context.get('data_interval_end').in_timezone('Asia/Seoul')
 
     slack_hook = SlackWebhookHook(slack_webhook_conn_id='conn_slack_airflow_bot')
-    slack_hook.send(text=f'{dag_id}.{task_id} 실패 알람, 에러내용:{err_msg}')
+    slack_hook.send(text=f'{dag_id}.{task_id} 실패 알람, 에러내용:{err_msg} / 배치일: {batch_date}')
