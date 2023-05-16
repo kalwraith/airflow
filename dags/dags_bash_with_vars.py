3dda329ba09123e2893c4095f75d35e40ab9aed2
hyunjinkim <kalwraith@naver.com>
2023-05-16 10:40:42 +0900

    vars 수정

--- dags/dags_bash_with_vars.py
+++ dags/dags_bash_with_vars.py
@@ -6,12 +6,15 @@ import pendulum
 
 value = Variable.get('sample_key')
 
-# plugins 폴더 공유를 통해 컨테이너 외부에 있는 shell 파일 수행하기
 with DAG(
     dag_id='dags_bash_with_vars',
-    start_date=pendulum.datetime(2023,3,1, tz='Asia/Seoul'),
-    catchup=False,
-    schedule='0 1 * * *'
+    start_date=pendulum.datetime(2023, 3, 1, tz='Asia/Seoul'),
+    end_date=pendulum.datetime(2023, 6, 1, tz='Asia/Seoul'),
+    catchup=True,
+    schedule='0 1 * * *',
+    default_args={
+        'retries': 1
+    }
 ) as dag:
     bash_bad_vars = BashOperator(
         task_id='bash_bad_vars',
